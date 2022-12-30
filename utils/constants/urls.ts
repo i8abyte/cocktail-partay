274152ba9823e1c7f6e0412ad73b7db9ebc00ad9
imran-salim <imransalim08@gmail.com>
2022-12-30 23:46:54 +1300

    Add URLs to access cocktails and ingredients from TheCocktailDB and a URL for Google searches

---
+++ utils/constants/urls.ts
@@ -0,0 +1,3 @@
+export const INGREDIENT_URL: string = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?i=';
+export const COCKTAIL_URL: string = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=';
+export const GOOGLE_SEARCH_URL: string = 'https://www.google.com/search?q=';
